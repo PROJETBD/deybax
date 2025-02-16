50fccdd2496563c338023c22642c61edd6307455
Mariama Diouf <dioufmariama0305@gmail.com>
2025-02-16 20:00:34 +0100

    Ajout du modele Parrainage

--- backend/laravel/app/Models/Parrainage.php
+++ backend/laravel/app/Models/Parrainage.php
@@ -8,4 +8,27 @@ use Illuminate\Database\Eloquent\Model;
 class Parrainage extends Model
 {
     use HasFactory;
+
+    protected $table = 'parrainages';
+
+    protected $fillable = [
+        'id_parrain',
+        'id_candidat',
+        'code_validation',
+        'date_parrainage'
+    ];
+
+    public $timestamps = false;
+
+    // Relation avec Parrain
+    public function parrain()
+    {
+        return $this->belongsTo(Parrain::class, 'id_parrain');
+    }
+
+    // Relation avec Candidat
+    public function candidat()
+    {
+        return $this->belongsTo(Candidat::class, 'id_candidat');
+    }
 }
