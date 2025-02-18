75697b79e72557b68188ce6526f8a5c7861c7460
unknown <julcisse00@gmail.com>
2025-02-18 00:07:28 +0000

    restructuration des controlleur

--- backend/laravel/app/Http/Controllers/CandidatController.php
+++ backend/laravel/app/Http/Controllers/CandidatController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Candidat;
 use Illuminate\Http\Request;
 
 class CandidatController extends Controller
 {
-    //
+    public function index()
+    {
+        return response()->json(Candidat::all());
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'electeur_id' => 'required|exists:electeurs,id|unique:candidats',
+            'nom_parti' => 'nullable|string',
+            'slogan' => 'nullable|string',
+            'photo' => 'nullable|string',
+            'couleurs' => 'nullable|string',
+            'url_info' => 'nullable|string',
+        ]);
+
+        $candidat = Candidat::create($request->all());
+        return response()->json($candidat, 201);
+    }
 }
